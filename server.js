27138fdacbb356754a1d1bcff6621b35f008d48c
Kobweb <yugoplixgamer@gmail.com>
2019-09-27 12:50:42 +0200

    Recovery of the different ingredients from the database to display them on the front

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const session = require('express-session');
+
+const nourriture = ['viande','supp','sauce','boisson','dessert'];
 
 const app = express();
 const connexion = mysql.createConnection({
@@ -13,8 +16,14 @@ const connexion = mysql.createConnection({
 });
 
 app.use(express.static(__dirname + '/public'));
+app.use(session({
+    secret: 'cowsnacksecuredtoken',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
-connexion.query('SELECT * FROM composition INNER JOIN purchase PC on composition.sandwich_id = PC.sandwich_id INNER JOIN stock ST on composition.ingredient_id = ST.ingredient_id', (err, rows) => {
+connexion.query('SELECT * FROM composition INNER JOIN purchase PC on composition.sandwich_id = PC.composition_id INNER JOIN stock ST on composition.ingredient_id = ST.id INNER JOIN size SZ on PC.size_id = SZ.id', (err, rows) => {
     if (err){
         console.log('une erreur est survenue !');
     } else {
@@ -24,16 +33,19 @@ connexion.query('SELECT * FROM composition INNER JOIN purchase PC on composition
         rows.forEach((result) => {
             if (result.sandwich_id > actual){
                 actual = result.sandwich_id;
-                count = 0;
+                count = [0,0,0,0,0];
                 commande[result.sandwich_id] = {
                     'id': result.sandwich_id,
                     'name': result.user_name,
-                    'phone': result.phone,
-                    'ingredients': {'ingredient 0': result.label}
+                    'phone': result.user_phone,
+                    'ingredients': {},
+                    'size': result.size_label
                 };
+                commande[result.sandwich_id]['ingredients'][nourriture[result.type-1]+count[result.type-1]] = result.label;
+                count[result.type-1]++;
             } else {
-                count++;
-                commande[result.sandwich_id]['ingredients']['ingredient '+parseInt(count)] = result.label;
+                commande[result.sandwich_id]['ingredients'][nourriture[result.type-1]+count[result.type-1]] = result.label;
+                count[result.type-1]++;
             }
         });
         console.log(commande);
@@ -41,11 +53,39 @@ connexion.query('SELECT * FROM composition INNER JOIN purchase PC on composition
 });
 
 app.get('/', (req, res) =>{
-    res.render('index.ejs');
+    res.render('countdown.ejs');
 })
-.post('/', urlencodedParser, (req, res) => {
+.get('/sandwich', (req, res) => {
+    connexion.query('SELECT * FROM stock', (err, rows) => {
+        res.render('sandwich.ejs', {print: rows, print2: rows, print3: rows, print4: rows});
+    });
+})
+.post('/sandwich', urlencodedParser, (req, res) => {
     console.log(req.body.postname);
     console.log(req.body.postclicked);
+})
+.get('/special', (req, res) => {
+    res.render('special.ejs');
+})
+.get('/apropos', (req, res) => {
+    res.render('apropos.ejs');
+})
+.get('/contact', (req, res) => {
+    res.render('contact.ejs');
+})
+.get('/conditionsgenerales', (req, res) => {
+    res.render('conditionsgenerales.ejs');
+})
+.get('/politique', (req, res) => {
+    res.render('politique.ejs')
+})
+.get('/mentionslegales', (req, res) => {
+    res.render('mentionslegales.ejs')
+})
+.use((req, res, next) => {
+    res.status(404).render('maintenance.ejs');
 });
 
 app.listen(8080);
+
+//SELECT * FROM composition INNER JOIN purchase PC on composition.sandwich_id = PC.composition_id INNER JOIN stock ST on composition.ingredient_id = ST.id INNER JOIN size SZ on PC.size_id = SZ.id
