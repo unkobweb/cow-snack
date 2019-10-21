0bf27eaf801ff844639eaf8a0192539755de9296
Kobweb <yugoplixgamer@gmail.com>
2019-10-21 20:55:02 +0200

    if you open a command describe panel, all other will be closed

--- public/js/admin.js
+++ public/js/admin.js
@@ -69,6 +69,9 @@ let customs = document.querySelectorAll(".colorSandwich");
 
 for (let i = 0; i < customs.length; i++) {
   customs[i].addEventListener("click", function(event) {
+    for (let i = 0; i < customs.length; i++) {
+      customs[i].nextElementSibling.classList.remove("custom");
+    }
     event.target.nextElementSibling.classList.toggle("custom");
     event.target.classList.toggle("presence");
   });
